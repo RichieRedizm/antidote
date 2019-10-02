e96887c136192fe5a32ce42ff4c5bc6f4cb0f6ce
Richie Redizm <richieredizm@gmail.com>
2019-10-02 09:16:21 +0100

    include additional comments

--- files/Framework.js
+++ files/Framework.js
@@ -1,38 +1,54 @@
-;(global => {
-	var Framework = input => {
-		return new Framework.init(input)
-	}
-	Framework.prototype = {
-		create: function() {
-			// remove hash as not required
-			const templateName = this.template.replace('#', '')
-			// get the template
-			const template = document.getElementById(templateName).innerHTML
-			// update placeholder with data
-			this.template = template.replace(/{{title}}/g, this.data.title)
-			return this
-		},
-		render: function(input) {
-			let content = document.querySelector(input)
-			content.innerHTML = this.template
-			this.addEvents()
-			return this
-		},
-		addEvents: function() {
-			const clear = document.querySelector('[data-event="click:clearTitle"]')
-			clear.addEventListener('click', this.clearTitle)
-			const keyUp = document.querySelector('[data-event="keyup:titleChanged"]')
-			keyUp.addEventListener('keyup', this.titleChanged)
-		}
-	}
-	Framework.init = function(input) {
-		let self = this
-		self.template = input.template || ''
-		self.data = input.data || ''
-		self.clearTitle = input.clearTitle || ''
-		self.titleChanged = input.titleChanged || ''
-	}
+/*
+/ initialise Framework
+*/
+const Framework = input => {
+	// create instance of Framework object
+	return new Framework.init(input)
+}
 
-	Framework.init.prototype = Framework.prototype
-	global.Framework = Framework
-})(window)
+/*
+/ protoype methods for Framework
+*/
+Framework.prototype = {
+	create: function() {
+		// remove hash as not required
+		const templateName = this.template.replace('#', '')
+		// get the template
+		const template = document.getElementById(templateName).innerHTML
+		// update placeholder with data
+		this.template = template.replace(/{{title}}/g, this.data.title)
+		return this
+	},
+	render: function(input) {
+		// get the container
+		let content = document.querySelector(input)
+		// set the template to the container
+		content.innerHTML = this.template
+		// add event listeners
+		this.addEvents()
+		return this
+	},
+	addEvents: function() {
+		// get the element for clear title and add click event listener
+		const clear = document.querySelector('[data-event="click:clearTitle"]')
+		clear.addEventListener('click', this.clearTitle)
+		// get the element for title changed and add key up event listener
+		const keyUp = document.querySelector('[data-event="keyup:titleChanged"]')
+		keyUp.addEventListener('keyup', this.titleChanged)
+	}
+}
+
+/*
+/ Structure of Framework object with defaults
+*/
+Framework.init = function(input) {
+	this.template = input.template || ''
+	this.data = input.data || ''
+	this.clearTitle = input.clearTitle || ''
+	this.titleChanged = input.titleChanged || ''
+}
+
+/*
+/ Add methods to Framework object
+*/
+Framework.init.prototype = Framework.prototype
